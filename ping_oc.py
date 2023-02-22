80859c91396c26616a984dc75468912b3f9337f0
ismaelbarbersanchezheredero <123732313+ismaelbarbersanchezheredero@users.noreply.github.com>
2023-02-22 20:04:14 +0100

    Comentados Cliente-Servidor Conectivo. Faltan caracteristicas+bucle

--- ping_oc.py
+++ ping_oc.py
@@ -3,9 +3,9 @@ import socket
 import time
 import numpy as np
 
-ip_serv= sys.argv[1]
-port_serv= int(sys.argv[2])
-address_serv=(ip_serv,port_serv)
+ip_serv= sys.argv[1] #Almacenamos en ip_serv el segundo parametro que pasamos al ejecutar el programa, que será la dir IP. (El primero es el nombre del programa)
+port_serv= int(sys.argv[2]) #Almacenamos en port_serv el tercer parametro que pasamos al ejecutar el programa, que será el puerto.
+address_serv=(ip_serv,port_serv) #Metemos en address_serv Dir IP y puerto en una sola cadena que usaremos para la funcion connect 
 tiempo_total=0
 
 #Comprobamos que se le han pasado el numero adecuado de argumentos
@@ -16,20 +16,20 @@ if(len(sys.argv)!=3):
 #Comprobacion del puerto adecuado
 
 
-#Creacion del socket
+#Creacion del socket. AF_INET indica uso de direcciones IPv4. SOCK_STREAM para que sea orientado a conexion
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('\nCreacion del socket')
 
-s.connect(address_serv) #Pasamos la direccionIP+Puerto del servidor al que quiere conectarse
+s.connect(address_serv) #A traves de address_serv indicamos a que dir IP y puerto queremos que se conecte
 print('\nConexion establecida')
 
 
 # Enviar datos al servidor
 message = "Hola, servidor!"
-s.send(message.encode())
+s.send(message.encode()) #Codificamos y enviamos el mensaje
 
 # Recibir datos del servidor
-data = s.recv(1024).decode()
+data = s.recv(1024).decode() #Utilizamos recv para recibir mensajes del servidor. Permitimos recibir hasta 1024 Bytes de datos de una sola vez.
 print("\nRespuesta del servidor:", data)
 
 # Cerrar la conexión
--- ping_oc_serv.py
+++ ping_oc_serv.py
@@ -2,9 +2,7 @@ import sys
 import socket
 
 port = int(sys.argv[1]) #Almacena en la variable port el parametro que se le pasa por teclado
-#ip_address = "192.168.43.187"
-#ip= socket.inet_aton(ip_address) #Me convierte la IP en una IP que puede entender el SO
-server_address = ('192.168.0.34', port)
+server_address = ('192.168.0.36', port)
 
 #Comprobamos que se le han pasado el numero adecuado de argumentos
 if(len(sys.argv)!=2):    
@@ -17,26 +15,27 @@ if(len(sys.argv)!=2):
        # print('El número de puerto del servidor escogido deber ser 1024-5000\n")
         #sys.exit()
 
-#Creacion del socket
+#Creacion del socket. AF_INET indica uso de direcciones IPv4. SOCK_STREAM para que sea orientado a conexion
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('\nCreacion del socket')
 
-#Asociacion del socket
+#Asociamos el socket a una dir IP y puerto especificos. Pasandole la cadena server_address que almacena dir IP y puerto del servidor  
 s.bind(server_address)
 print('\nAsociacion del socket')
 
 #i=0
 print('\nEsperando escuchas\n')
 
-s.listen(5) #Permite hasta 5 conexiones en espera
+s.listen(5) #Indicamos que permita hasta 5 conexiones en espera/cola
 
 while True:
+    #Accept acepta una conexion entrante. Devuelve un socket nuevo. Y la dir IP y puerto del cliente que se ha conectado, que almacenamos en client_address
     client_socket, client_address = s.accept() #Genera socket nuevo client_socket. client_address almacena la dir IP y puerto del cliente
     print('\nSe ha conectado el cliente ',client_address)
 
-    data = client_socket.recv(1024) #Recibe hasta 1024bytes de datos y los almacena en la variable data
+    data = client_socket.recv(1024) #Utilizamos recv para recibir mensajes del cliente. Permitimos recibir hasta 1024 Bytes de datos de una sola vez.
     print('\n',data.decode())
 
     mensaje = "Hola cliente, te hablo, soy el servidor. Encantado"
-    client_socket.send(mensaje.encode())
+    client_socket.send(mensaje.encode())    #Codificamos y enviamos el mensaje
     print('\nMensaje enviado')
